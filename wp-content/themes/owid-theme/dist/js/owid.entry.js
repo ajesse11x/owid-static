4e4da67c53b6ea73b8c076fd1b876270c3f06d34
OWID <owid@ourworldindata.org>
2019-01-07 18:41:38 +0000

    Code deployment update

--- wp-content/themes/owid-theme/dist/js/owid.entry.js
+++ wp-content/themes/owid-theme/dist/js/owid.entry.js
@@ -3,13 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("../css/style.scss");
 require("./oldScripts.js");
 var Analytics_1 = require("./Analytics");
+Analytics_1.Analytics.logEvent("OWID_PAGE_LOAD", { href: window.location.href, title: document.title });
 var search = document.querySelector("form#search-nav");
 if (search) {
     var input_1 = search.querySelector("input[type=search]");
     var lastQuery = "";
     search.addEventListener('submit', function (ev) {
         ev.preventDefault();
-        Analytics_1.Analytics.logEvent("OWID_SITE_SEARCH", { query: input_1.value }).then(function () { return search.submit(); }).catch(function () { return search.submit(); });
+        Analytics_1.Analytics.logEvent("OWID_SITE_SEARCH", { query: input_1.value, href: window.location.href }).then(function () { return search.submit(); }).catch(function () { return search.submit(); });
     });
 }
 //# sourceMappingURL=owid.entry.js.map
